7f538a7c3c374c66200d8597821b3b178832f382
BrittniLudington <brittniludington@gmail.com>
2019-07-29 21:32:32 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Cursor Tail
+
+Have a trail of squares follow cursor around on canvas
+
+## Purpose
+
+To utilize a linked list in the scheme of animation.
---
+++ tail.js
@@ -0,0 +1,16 @@
+
+let canvas;
+let context;
+function setUp()
+{
+    canvas = document.getElementById("canvas");
+    context = canvas.getContext("2d");
+    requestAnimationFrame(draw);
+}
+
+function draw()
+{
+
+}
+
+setUp();
